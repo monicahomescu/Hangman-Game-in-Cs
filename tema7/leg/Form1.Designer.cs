b08df55daea3bd6f536c83256409e288e3795d98
Monica Homescu <monihomescu@yahoo.com>
2022-12-17 09:36:07 +0200

    Add files via upload

---
+++ tema7/leg/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace leg
+{
+    partial class leg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.btn = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Location = new System.Drawing.Point(12, 60);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(310, 378);
+            this.pnl.TabIndex = 0;
+            this.pnl.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_Paint);
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.LightCoral;
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn.Location = new System.Drawing.Point(372, 25);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(71, 30);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "start";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txt1.Location = new System.Drawing.Point(375, 114);
+            this.txt1.Name = "txt1";
+            this.txt1.PasswordChar = '*';
+            this.txt1.Size = new System.Drawing.Size(139, 21);
+            this.txt1.TabIndex = 2;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txt2.Location = new System.Drawing.Point(585, 114);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(25, 21);
+            this.txt2.TabIndex = 3;
+            this.txt2.Visible = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.LightCoral;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn2.Location = new System.Drawing.Point(372, 264);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(71, 27);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "exit";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(382, 173);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 5;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn3.Location = new System.Drawing.Point(628, 113);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(52, 24);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "next";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Visible = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txt3.Location = new System.Drawing.Point(585, 236);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 21);
+            this.txt3.TabIndex = 7;
+            this.txt3.Visible = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn4.Location = new System.Drawing.Point(700, 231);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 32);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "answer";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Visible = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // txt4
+            // 
+            this.txt4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txt4.Location = new System.Drawing.Point(568, 338);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(124, 23);
+            this.txt4.TabIndex = 9;
+            this.txt4.Visible = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl2.Location = new System.Drawing.Point(372, 85);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(122, 17);
+            this.lbl2.TabIndex = 10;
+            this.lbl2.Text = "Cuvantul introdus:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl3.Location = new System.Drawing.Point(582, 85);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(48, 17);
+            this.lbl3.TabIndex = 11;
+            this.lbl3.Text = "Litera:";
+            this.lbl3.Visible = false;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl4.Location = new System.Drawing.Point(582, 207);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(98, 17);
+            this.lbl4.TabIndex = 12;
+            this.lbl4.Text = "Raspuns final:";
+            this.lbl4.Visible = false;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lbl5.ForeColor = System.Drawing.Color.Red;
+            this.lbl5.Location = new System.Drawing.Point(434, 338);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(128, 18);
+            this.lbl5.TabIndex = 13;
+            this.lbl5.Text = "Raspunsul corect:";
+            this.lbl5.Visible = false;
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl6.Location = new System.Drawing.Point(582, 150);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(0, 17);
+            this.lbl6.TabIndex = 14;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.LightCoral;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn5.Location = new System.Drawing.Point(700, 400);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 28);
+            this.btn5.TabIndex = 15;
+            this.btn5.Text = "reset";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Font = new System.Drawing.Font("Bauhaus 93", 16F);
+            this.lbl7.Location = new System.Drawing.Point(69, 26);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(187, 25);
+            this.lbl7.TabIndex = 16;
+            this.lbl7.Text = "SPANZURATOAREA";
+            // 
+            // leg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pnl);
+            this.Name = "leg";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.leg_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Label lbl7;
+    }
+}
+
